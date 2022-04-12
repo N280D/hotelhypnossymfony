932bf14e3d3f335246567fd0e7f580daeaaea7c3
NAWEL <naowel@hotmail.fr>
2022-04-12 16:38:05 +0200

    update regiter form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('Nom')
+            ->add('Prenom')
+        
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
