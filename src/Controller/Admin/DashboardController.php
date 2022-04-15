b5a48863af36604cd946ad2844d6439ea8c50487
NAWEL <naowel@hotmail.fr>
2022-04-15 15:51:15 +0200

    url dashboard set up ok

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,21 +4,35 @@ namespace App\Controller\Admin;
 
 use App\Entity\Suite;
 use App\Entity\Etablissement;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
+
 {
+
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+        ){
+         }
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
+     $url=$this->adminUrlGenerator
+     ->setController(SuiteCrudController::class)
+     ->generateUrl();
+     
+     return $this->redirect($url);
+    }
+     
+     //return parent::index();
+        //return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
         // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
+        
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
 
@@ -32,7 +46,7 @@ class DashboardController extends AbstractDashboardController
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
         // return $this->render('some/path/my-dashboard.html.twig');
-    }
+    
 
     public function configureDashboard(): Dashboard
     {
