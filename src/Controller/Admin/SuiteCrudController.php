b70e4040f77f727f0e02f330518c9a4e5bda2bd9
NAWEL <naowel@hotmail.fr>
2022-04-15 14:19:00 +0200

    mise en place du cruud admin

---
+++ src/Controller/Admin/SuiteCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Suite;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+
+class SuiteCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Suite::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('titre'),
+            TextEditorField::new('description'),
+            MoneyField::new('prix')->setCurrency('EUR'),
+            ImageField::new('image')->setBasePath('/assets/uploads/products/')
+
+        ];
+    }
+    
+}
